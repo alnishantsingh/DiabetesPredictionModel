b3df8006dc5b68694b9f31589d6ab70689df4aae
Rohan Banduni <127114421+rohanbanduni@users.noreply.github.com>
2023-12-10 12:01:21 +0530

    Add files via upload

---
+++ Project/app.py
@@ -0,0 +1,49 @@
+import pickle
+import os
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+base_path = os.path.abspath(os.path.dirname(__file__))
+model_path = os.path.join(base_path, 'pipe.pkl')
+
+with open(model_path, 'rb') as model_file:
+    model = pickle.load(model_file)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    if request.method == 'POST':
+        user_input = []
+        features = [
+            'HighBP', 'HighChol', 'CholCheck', 'BMI', 'Smoker', 'Stroke', 'HeartDiseaseorAttack',
+            'PhysActivity', 'Fruits', 'Veggies', 'HvyAlcoholConsump', 'AnyHealthcare', 'NoDocbcCost',
+            'GenHlth', 'MentHlth', 'PhysHlth', 'DiffWalk', 'Sex', 'Age', 'Education', 'Income'
+        ]
+        for feature in features:
+            user_input.append(int(request.form.get(feature)))
+
+        prediction = model.predict([user_input])
+        return render_template('predict.html', prediction=prediction[0])
+
+    return render_template('test.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/predict')
+def predict():
+    return render_template('predict.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
